4671e91a5364290a2ec6c7c59785cc3499a8c913
SennayT <wsennay@gmail.com>
2021-11-14 11:14:48 +0300

    Initial Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,129 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h4>The score is {{ average }}</h4>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="2">
+        <v-row>
+          <v-col cols="6">
+            <p>Name</p>
+          </v-col>
+          <v-col cols="6">
+            <p>num</p>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="5">
+        <p>Score</p>
+      </v-col>
+      <v-col cols="5">
+        <p>Weight</p>
+      </v-col>
+    </v-row>
+    <v-row align="center" v-for="(item, index) in scores" :key="index">
+      <v-col cols="2">
+        <v-row>
+          <v-col cols="6">
+            <p>{{ item.name }} (x {{ item.numberOfItems }})</p>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field v-model="item.numberOfItems"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="5">
+        <v-row align="center">
+          <v-col cols="6">
+            <v-slider min="0" :max="item.max" v-model="item.score"></v-slider>
+          </v-col>
+          <v-col cols="3">
+            <v-text-field v-model="item.score" type="number"></v-text-field>
+          </v-col>
+          <v-col cols="3"
+            >{{ Math.round((item.score / item.max) * 100) }}%</v-col
+          >
+        </v-row>
+      </v-col>
+      <v-col cols="5">
+        <v-row class="align-center">
+          <v-col cols="9">
+            <v-slider min="0" max="100" v-model="item.weight"></v-slider>
+          </v-col>
+          <v-col cols="3">
+            <v-text-field v-model="item.weight" type="number"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <h4>The score is {{ average }}</h4>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  data: () => {
+    return {
+      elevation: 1,
+      scores: [
+        {
+          name: "Quiz",
+          score: 8,
+          max: 10,
+          weight: 10,
+          numberOfItems: 22,
+        },
+        {
+          name: "Inception Report",
+          score: 9.23,
+          max: 15,
+          weight: 15,
+          numberOfItems: 1,
+        },
+        {
+          name: "Project Management Plan",
+          score: 10,
+          max: 15,
+          weight: 15,
+          numberOfItems: 1,
+        },
+        {
+          name: "Design Plan",
+          score: 10,
+          max: 15,
+          weight: 15,
+          numberOfItems: 1,
+        },
+        {
+          name: "Final",
+          score: 23,
+          max: 40,
+          weight: 40,
+          numberOfItems: 1,
+        },
+      ],
+    };
+  },
+  computed: {
+    average() {
+      let totalScore = 0;
+      let totalWeight = 0;
+      this.scores.forEach((score) => {
+        totalWeight = totalWeight + score.weight * score.numberOfItems;
+        const averageScore = (score.score / score.max) * 100;
+        console.log(`The average for ${score.name} is ${averageScore}`);
+        totalScore =
+          totalScore + averageScore * score.weight * score.numberOfItems;
+      });
+      return totalScore / totalWeight;
+    },
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
